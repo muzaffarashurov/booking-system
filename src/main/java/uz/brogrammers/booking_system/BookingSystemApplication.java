535825d6b58298cef27c72894d2283eb97e626e0
MuzaffarAshurov <muzaffarashurov1982@gmail.com>
2026-04-16 22:39:59 +0500

    Some annotations have been added to the BookingSystemApplication.java file.

--- src/main/java/uz/brogrammers/booking_system/BookingSystemApplication.java
+++ src/main/java/uz/brogrammers/booking_system/BookingSystemApplication.java
@@ -2,12 +2,16 @@ package uz.brogrammers.booking_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class BookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemApplication.class, args);
+    }
 
 }
